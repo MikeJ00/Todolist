058934d2f38f3b06c5541d3f24cdeadd1ec22ba6
Mike-casper <misha7709123@gmail.com>
2022-12-26 16:49:02 +0300

    работает добавление и удаление тасок

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Todolist} from './Todolist';
-import { v1 } from 'uuid';
+import React, {useReducer, useState} from "react";
+import "./App.css";
+import {Todolist} from "./Todolist";
+import {v1} from "uuid";
+import {addTaskAC, removeTaskAC, tasksReducer} from "./reducer/tasksReducer";
+
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    let [tasks, setTasks] = useState([
-        { id: v1(), title: "HTML&CSS", isDone: true },
-        { id: v1(), title: "JS", isDone: true },
-        { id: v1(), title: "ReactJS", isDone: false },
-        { id: v1(), title: "Rest API", isDone: false },
-        { id: v1(), title: "GraphQL", isDone: false },
-    ]);
+    // let [tasks, setTasks] = useState([
+    //     { id: v1(), title: "HTML&CSS", isDone: true },
+    //     { id: v1(), title: "JS", isDone: true },
+    //     { id: v1(), title: "ReactJS", isDone: false },
+    //     { id: v1(), title: "Rest API", isDone: false },
+    //     { id: v1(), title: "GraphQL", isDone: false },
+    // ]);
+
+    let [tasks, dispatchTasks] = useReducer(tasksReducer, [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false},
+    ])
 
     function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
+        // let filteredTasks = tasks.filter(t => t.id != id);
+        // setTasks(filteredTasks);
+        dispatchTasks(removeTaskAC(id));
     }
 
     function addTask(title: string) {
-        let task = { id: v1(), title: title, isDone: false };
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);
+        // let task = {id: v1(), title: title, isDone: false};
+        // let newTasks = [task, ...tasks];
+        // setTasks(newTasks);
+        dispatchTasks(addTaskAC(title))
     }
 
     let [filter, setFilter] = useState<FilterValuesType>("all");
@@ -42,44 +54,18 @@ function App() {
     }
 
 
-
     return (
         <div className="App">
             <Todolist title="What to learn"
                       tasks={tasksForTodolist}
                       removeTask={removeTask}
                       changeFilter={changeFilter}
-                      addTask={addTask} />
+                      addTask={addTask}/>
         </div>
     );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //After 7 lesson------------------------------------//
