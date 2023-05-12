a1121a7506f844ebc9b29fa3c4ffa1d47b80350a
Mike-casper <misha7709123@gmail.com>
2023-05-12 19:10:48 +0300

    #7 add Reducer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,21 @@
  import React from 'react';
-// import styles from "../Todolist.module.css";
-// import {FilterValueType} from "../App";
-//
-// type buttonPropsType={
-//     name:string
-//     callBack:()=>void
-//     className?:any
-//     Filter?:FilterValueType
-//     // универсальная функция callback, для того чтобы в этой фенкции
-//     // было 3 других функции addTaskHandler removeTaskHandler filterTasks
-// }
-// export const Button=(props:buttonPropsType)=>{
-//     const onClickHandler=()=>{
-//         props.callBack()
-//
-//     }
-//     return(
-//         <button className={props.name===props.Filter? styles.activeFilter : " "} onClick={onClickHandler}>{props.name}</button>
-//     )
-// }
+import styles from "../Todolist.module.css";
+import {FilterValueType} from "../App";
+
+type buttonPropsType={
+    name:string
+    callBack:()=>void
+    className?:any
+    Filter?:FilterValueType
+    // универсальная функция callback, для того чтобы в этой фенкции
+    // было 3 других функции addTaskHandler removeTaskHandler filterTasks
+}
+export const Button=(props:buttonPropsType)=>{
+    const onClickHandler=()=>{
+        props.callBack()
+
+    }
+    return(
+        <button className={props.name===props.Filter? styles.activeFilter : " "} onClick={onClickHandler}>{props.name}</button>
+    )
+}
--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -1,23 +1,50 @@
 import React, {useState} from "react";
-// import {TaskType} from "../Todolist";
+import {TaskType} from "../Todolist";
+import {v1} from "uuid";
 
-// export const tasksReducer=(state:TaskType[], action:TsarType)=>{
-//    switch (action.type){
-//        case "REMOVE_TASK":{
-//            return state.filter(t=>t.id!==action.payload.todolistID)
-//        }
-//        // default:return console.log("Vse propalo") 1 варинт написания
-//        default: return state
-//    }
-// }
-// type TsarType=RemoveTaskACtype
-// export type RemoveTaskACtype=ReturnType<typeof removeTaskAC>
-//
-// export const removeTaskAC=(todolistID:string)=>{
-//     return{
-//         type:"REMOVE_TASK",
-//         payload:{
-//             todolistID
-//         }
-//     } as const
-// }
+type RootType = removeTasksACType | addTaskReturnACType | changeCheckBoxReturnACType
+type removeTasksACType = ReturnType<typeof removeTasksAC>
+type addTaskReturnACType = ReturnType<typeof addTaskAC>
+type changeCheckBoxReturnACType = ReturnType<typeof changeCheckBoxAC>
+
+export const tasksReducer= (state:TaskType[], action:RootType)=>{
+    switch (action.type) {
+        case "REMOVE-TASK":{
+            return state.filter(el=>el.id !== action.payload.taskID)
+        }
+        case "ADD-TASK":{
+            let newTask={ id: v1(), title: action.payload.newTitle, isDone: false }
+            return [...state, newTask]
+        }
+        case "CHANGE-CHECKBOX":{
+            return state.map((el => el.id === action.payload.taskID ? {...el, isDone: action.payload.eventValue} : el))
+
+        }
+        default: return state
+    }
+}
+export const removeTasksAC = (taskID:string) =>{
+    return {
+        type:"REMOVE-TASK",
+        payload:{
+            taskID
+        }
+    } as const
+}
+export const addTaskAC = (newTitle:string) => {
+    return {
+        type: "ADD-TASK",
+        payload:{
+            newTitle
+        }
+    } as const
+}
+export const changeCheckBoxAC = (taskID:string, eventValue:boolean) =>{
+    return {
+        type: "CHANGE-CHECKBOX",
+        payload: {
+            taskID,
+            eventValue
+        }
+    }as const
+}
